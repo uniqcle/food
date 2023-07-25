830c5a85168c91eb8e530d5c6f062b768eec68cc
uniqcle <uniqcle@yandex.ru>
2023-07-25 11:12:59 +0700

    timer

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
-
+    /////////////////////////////////////////////////
+    // Tabs
+    /////////////////////////////////////////////////
     const tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll('.tabcontent'),
         tabsParent = document.querySelector('.tabheader__items');
@@ -19,7 +21,6 @@ window.addEventListener('DOMContentLoaded', () => {
         tabs[tabNumber].classList.add('tabheader__item_active')
     }
 
-
     hideTabContent();
     showTabContent(2);
 
@@ -36,4 +37,59 @@ window.addEventListener('DOMContentLoaded', () => {
         }
 
     })
+
+    /////////////////////////////////////////////////
+    // Timer
+    /////////////////////////////////////////////////
+    const deadLine = '2024-05-27';
+
+    // разница между deatLine и текущ. временем
+    function getTimeRemaining(endTime) {
+        const t = Date.parse(endTime) - Date.parse(new Date()),
+            days = Math.floor(t / (1000 * 60 * 60 * 24)), // Кол-во дней.  миллисекунд в сутках
+            hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+            mins = Math.floor((t / 1000 / 60) % 60),
+            secs = Math.floor((t / 1000) % 60);
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'mins': mins,
+            'secs': secs
+        }
+    }
+
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`
+        }
+        return num
+    }
+
+    function setClock(selector, endTime) {
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            mins = timer.querySelector('#minutes'),
+            secs = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock() {
+            const t = getTimeRemaining(endTime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            mins.innerHTML = getZero(t.mins);
+            secs.innerHTML = getZero(t.secs);
+
+            if (t.total <= 0) clearInterval(timeInterval) // если время вышло, таймер останавливаем
+        }
+    }
+
+    setClock('.timer', deadLine);
+
+
 })
