8c9bd665b90a0c921d49e9d0c0aa9a5c2cf51056
uniqcle <uniqcle@yandex.ru>
2023-07-25 15:47:44 +0700

    modal modified

--- js/script.js
+++ js/script.js
@@ -102,4 +102,56 @@ window.addEventListener('DOMContentLoaded', () => {
     setClock('.timer', deadLine);
 
 
+    /////////////////////////////////////////////////
+    // Modal 
+    /////////////////////////////////////////////////
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+        modal = document.querySelector('.modal'),
+        btnModalClose = document.querySelector('[data-close]');
+
+    function closeModal() {
+        modal.style.display = 'none';
+        document.body.style.overflow = ''
+    }
+
+    function openModal() {
+        modal.style.display = 'block'
+        document.body.style.overflow = 'hidden'
+        clearInterval(modalTimerId)
+    }
+    // навешиваем события на кнопки
+    modalTrigger.forEach(trigger => {
+        trigger.addEventListener('click', function () {
+            openModal()
+        })
+    });
+
+    btnModalClose.addEventListener('click', closeModal);
+
+    //если кликает по области вне модальн. окна
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            closeModal();
+        }
+    });
+
+    // закрываем по клавише esc
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape') {
+            closeModal();
+        }
+    })
+
+    // открываем модальн. окное по истечении 2 сек. 
+    const modalTimerId = setTimeout(openModal, 2000);
+
+    function showModalByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) { // означает что пользователь долистал до конца страницы.
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll)
+        }
+    }
+
+    window.addEventListener('scroll', showModalByScroll)
+
 })
